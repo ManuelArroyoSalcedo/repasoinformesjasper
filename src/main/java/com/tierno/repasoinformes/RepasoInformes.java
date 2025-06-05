3776a09a588c9f791d152654a4b04a367992abd7
Fulanito de Tal <fulanitodetal@ejemplo.com>
2025-06-05 19:24:04 +0200

    Subo el proyecto por primera vez al GitHub

---
+++ src/main/java/com/tierno/repasoinformes/RepasoInformes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tierno.repasoinformes;
+
+/**
+ *
+ * @author Manuel
+ */
+public class RepasoInformes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
